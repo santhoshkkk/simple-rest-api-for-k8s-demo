849d25c5f10c5122d819aac2c4d865dffca9e424
santhoshkkk <sannthoshkumar.krishnakurup@gmail.com>
2020-09-24 14:25:07 +0530

    docker-compose

--- src/main/kotlin/com/santhoshkkk/simpleapi/ApiController.kt
+++ src/main/kotlin/com/santhoshkkk/simpleapi/ApiController.kt
@@ -2,11 +2,12 @@ package com.santhoshkkk.simpleapi
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import kotlin.random.Random
 
 @RestController
 class ApiController {
 
     @GetMapping
-    fun get() = "Welcome to kubernetes"
+    fun get() = "Hi there.Your lucky number is ${Random.nextInt()}"
 
 }
